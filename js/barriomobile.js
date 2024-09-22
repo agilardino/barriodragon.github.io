b91af8c46c6b9365a9a35a78cd8551f500314e8f
agilardino <alejandro.gilardino@gmail.com>
2024-09-21 21:49:00 -0500

    Add files via upload

---
+++ js/barriomobile.js
@@ -0,0 +1,33 @@
+(function (a, b) {
+    if (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(a)) {
+        window.location = b;
+    }
+})(navigator.userAgent || navigator.vendor || window.opera, 'https://barrio-dragon.s3.amazonaws.com/mobile/index.html');
+
+function isSafariOniPhone() {
+    return /iPhone|iPad|iPod/i.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+}
+
+if (isSafariOniPhone()) {
+    window.location.href = "https://barrio-dragon.s3.amazonaws.com/mobile/index.html";
+}
+
+function isMobileOrTablet() {
+    return $(window).width() <= 1080; // Adjust the screen width threshold as needed 768
+}
+
+function redirectIfMobileOrTablet() {
+    if (isMobileOrTablet()) {
+        window.location.href = "https://barrio-dragon.s3.amazonaws.com/mobile/index.html";
+    }
+}
+
+$(document).ready(function () {
+    // Check and redirect on page load
+    redirectIfMobileOrTablet();
+
+    // Check and redirect when the window is resized
+    $(window).resize(function () {
+        redirectIfMobileOrTablet();
+    });
+});
